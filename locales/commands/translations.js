01c97ea5c1879d04afcf0b5a9fb0c938942763ec
NullDev (Shadow) <chris@nulldev.org>
2023-06-01 20:28:55 +0200

    Base locales

---
+++ locales/commands/translations.js
@@ -0,0 +1,34 @@
+export default {
+    help: {
+        desc: "Show an overview of commands.",
+        translations: {
+            de: "Zeig eine Übersicht aller Befehle.",
+        },
+    },
+    info: {
+        desc: "Show information about this bot.",
+        translations: {
+            de: "Zeig Informationen über diesen Bot.",
+        },
+    },
+    admin_help: {
+        desc: "Show an overview of all admin commands.",
+        translations: {
+            de: "Zeig eine Übersicht aller Admin-Befehle.",
+        },
+    },
+    set_language: {
+        desc: "Set the server language for the bot.",
+        translations: {
+            de: "Setze die Server-Sprache für den Bot.",
+        },
+        options: {
+            language: {
+                desc: "The language to set",
+                translations: {
+                    de: "Die zu setzende Sprache",
+                },
+            },
+        },
+    },
+};
